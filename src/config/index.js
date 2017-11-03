cbde2f30aba2022746c089477d0df7695bdecba2
DESKTOP-73RF5IL\rm <763457149@qq.com>
2017-11-03 16:53:13 +0800

    古典音乐广播

--- src/config/index.js
+++ src/config/index.js
@@ -1,48 +1,48 @@
 //配置文件
 //channelLogo
-const logoIndex = 9
+const logoIndex = 10
 const CHANNEL_LOGO = require(`../logo/${logoIndex}.png`)
     //封面
-const CHANNEL_TITLE = '乐龄105.6';
+const CHANNEL_TITLE = '古典音乐广播';
 //one
 const CHANNEL_AUDIO = 'http://www.hndt.com/podcast/881/1026/201710/31/1914736/res/DXIxLN8m.mp3';
 
-const CHANNEL_AUDIO_TEXT_1 = '河南广播电视台信息广播（FM105.6/AM603），前身为河南信息广播电台，开播于1994年6月8日，凭借调频、中波、卫星、网络，覆盖河南全省及周边3亿多听众，是中原地区极具成长性的广播媒体。';
+const CHANNEL_AUDIO_TEXT_1 = '2015年8月8日，古典音乐广播（936天籁之音）开播，一首首动听的中外经典名曲，通过FM93.6的电波流入人们心扉。';
 
-const CHANNEL_AUDIO_TEXT_2 = '2015年7月，信息广播改版升级为信息广播“乐龄105.6”，开创全国省级中老年广播先河。';
+const CHANNEL_AUDIO_TEXT_2 = '古典音乐广播是一家以播出古典音乐为主的专业调频广播，旨在满足不同听众的需求，提升大众文化素养，普及高雅音乐，陶冶人们情操，助力“文明河南”建设。';
 
 //two
-const CHANNEL_VIDEO = 'http://hndt.com/fm/1066/201708/07/1897792/res/vk2yJveQ.mp4';
+const CHANNEL_VIDEO = 'http://hndt.com/h5/shows/10/videos/1.mp4';
 
-const CHANNEL_VIDEO_TEXT_1 = '以“点亮多彩生活，陪伴不老青春”为口号，以“年轻态”为频率风格主调，全方位打造国内中老年广播第一品牌。“乐龄105.6”节目设置以“新闻资讯、生活服务、文艺娱乐”为主线，从生活保障、文化娱乐，到精神抚慰、时尚引导，全方位服务中老年人群。';
+const CHANNEL_VIDEO_TEXT_1 = '936天籁之音的节目理念是“严肃音乐、通俗传播”。古典音乐广播充分考虑到当代听众的实际需求和收听习惯，标准化、格式化编排，将高雅、经典纯音乐广播化、碎片化呈现，易于车载移动收听。';
 
 //three
 const CHANNEL_PICS = [{
-        text: '新时代新乐龄！ 乐龄1056情暖重阳系列活动开始啦',
-        url: 'http://www.hndt.com/fm/1056/201710/24/1913444/res/dvHJN7gg.jpg'
+        text: '2017漫步经典：国家大剧院管弦乐团“夏夜传说”',
+        url: 'http://www.hndt.com/fm/936/1892690/res/vnzDzPEQ.jpg?1499738288241'
     },
     {
-        text: '“中原乐龄展风采 喜迎党的十九大”活动完美收官！',
-        url: 'http://www.hndt.com/fm/1056/201709/22/1906988/res/69EdxlxK.jpg'
+        text: '郑州爱乐轻音乐团三周年音乐会周日上演',
+        url: 'http://www.hnr.cn/broadcast/fm936/jdt/201609/W020160923570219907672.jpg'
     },
     {
-        text: '《名医在线》联合郑大一附院举行大型义诊活动',
-        url: 'http://www.hndt.com/fm/1056/201710/26/1913813/res/N6ZzekmJ.jpg'
+        text: '大型环球经典音乐会《东方之声》在郑演出',
+        url: 'http://www.hndt.com/fm/936/6980/res/drylOle9.jpg?1487668875672'
     }
 ]
 
 //four
 const CHANNEL_PICS_2 = [{
-        text: '新时代，新乐龄！“乐游明清古村”听众报名火爆进行中',
-        url: 'http://www.hndt.com/fm/1056/201710/24/1913426/res/P9kIZtri.jpg'
+        text: '西方古典音乐家的电影之《魔鬼小提琴家帕格尼尼》',
+        url: 'http://www.hndt.com/fm/936/201710/31/1914681/res/RGP9cdil.png?1509495598313'
     },
     {
-        text: '“我是小小急救员” 暑期急救自救技能大练兵',
-        url: 'http://www.hndt.com/fm/1056/201709/03/1903238/res/fsEssF0i.jpg'
+        text: '经典原版音乐剧《人鬼情未了》即将在郑重现',
+        url: 'http://www.hnr.cn/broadcast/fm936/jdt/201611/W020161115387313797185.jpg'
     },
     {
-        text: '红姐配餐进社区，做蛋糕这事，不难，很快乐！',
-        url: 'http://www.hndt.com/fm/1056/201709/03/1903236/res/FGEBjLuB.jpg'
+        text: '国家大剧院制作歌剧《图兰朵》',
+        url: 'http://www.hnr.cn/broadcast/fm936/jdt/201702/W020170208589806209080.jpg'
     }
 ]
 
