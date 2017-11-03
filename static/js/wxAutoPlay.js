394fa609b758cb00d3653b99984a52db8351fb9c
DESKTOP-73RF5IL\rm <763457149@qq.com>
2017-11-03 17:53:21 +0800

    古典音乐配置

--- static/js/wxAutoPlay.js
+++ static/js/wxAutoPlay.js
@@ -1,6 +1,6 @@
 window.onload = function() {
     var TITLE = '2018推介会'
-    var LINK = 'http://hndt.com/h5/vlive/index.html'
+    var LINK = 'http://hndt.com/h5/shows/10/index.html'
     var IMGURL = 'http://hndt.com/res/logo_300.png'
     var DESC = '2018河南广播电视台广播品牌推介会'
 
