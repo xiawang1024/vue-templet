0f2b6b629267597fdeadcdc8e4b8f1cdefde3edc
DESKTOP-73RF5IL\rm <763457149@qq.com>
2017-11-03 15:47:52 +0800

    私家车广播

--- src/config/index.js
+++ src/config/index.js
@@ -1,49 +1,49 @@
 //配置文件
 //channelLogo
-const logoIndex = 5
+const logoIndex = 7
 const CHANNEL_LOGO = require(`../logo/${logoIndex}.png`)
     //封面
-const CHANNEL_TITLE = '河南农村广播';
+const CHANNEL_TITLE = '私家车广播';
 //one
 const CHANNEL_AUDIO = 'http://www.hndt.com/podcast/881/1026/201710/31/1914736/res/DXIxLN8m.mp3';
 
-const CHANNEL_AUDIO_TEXT_1 = '农村广播是国家批准的政策性广播、公益性媒体。2015年9月1日全新改版，定位于“沟通城乡，服务三农”，服务城乡居民生产生活每一天。农村广播以互联网新媒体思维办节目，节目不但形式新颖，服务性、权威性强，而且线上节目播出，线下活动带动，节目一次采集、多元生成、多平台传播，是河南惟一一家面向“ 三农”、 专业服务7000万城乡人口的专业电台。';
+const CHANNEL_AUDIO_TEXT_1 = '私家车广播是隶属于河南广播电视台的专业广播。私家车广播的前身是旅游广播。2012年，经过华丽转身和蜕变，这家城市广播成为河南省第一家“私家车电台”。私家车999—一个全新的呼号，成为河南广播一个响亮的名字、闪光的品牌。';
 
-const CHANNEL_AUDIO_TEXT_2 = '';
+const CHANNEL_AUDIO_TEXT_2 = '私家车广播是一家有品位、有追求、有责任、有影响的省级电台。';
 
 //two
-const CHANNEL_VIDEO = 'http://www.hndt.com/fm/1074/201708/07/1897788/res/u9M50TSH.mp4';
+const CHANNEL_VIDEO = 'http://hndt.com/fm/999/201708/07/1897791/res/kXhacpsr.mp4';
 
-const CHANNEL_VIDEO_TEXT_1 = '10多年来，农村广播紧扣“三农”需求，强化“农”字招牌，成为城乡农民生产生活的好帮手、好顾问、好参谋，为推动我省农业发展、社会和谐、建设美丽富裕新农村做出应有贡献。';
+const CHANNEL_VIDEO_TEXT_1 = '高速发展的城市交通、日益壮大的私家车人群，使广播再现辉煌，重回强势媒体的同时，催生了“私家车广播FM99.9”，成为我省广播史上第一个定位私家车的广播频率。';
 
 
 //three
 const CHANNEL_PICS = [{
-        text: '助力精准扶贫 专家服务团走进“第一书记”扶贫村',
-        url: 'http://www.hndt.com/fm/1074/201710/27/1913891/res/hiKYbqK9.jpg'
+        text: '中国好司机之高速公路亿万公里零违法挑战赛在郑州启动',
+        url: 'http://www.hndt.com/fm/999/201708/08/1898108/res/c3MggwnE.jpg?1502414489407'
     },
     {
-        text: '栾川竟藏着这种好东西，十四国总理都为它点赞！',
-        url: 'http://www.hndt.com/fm/1074/201710/30/1914192/res/EW8lQhGx.jpg'
+        text: '私家车999联合通视传媒送出万元七夕大礼包',
+        url: 'http://www.hndt.com/fm/999/201708/28/1901646/res/wBTeOumR.jpg?1503888293153'
     },
     {
-        text: '第一书记富民成果走进雏牧香 河南广电扶贫再发力',
-        url: 'http://www.hndt.com/fm/1074/201710/16/1911320/res/1aZCF3fB.jpg'
+        text: '私家车999第十届时光音乐节',
+        url: 'http://www.hndt.com/fm/999/201709/25/1907368/res/o00Q8ycD.jpg'
     }
 ]
 
 //four
 const CHANNEL_PICS_2 = [{
-        text: '《打工直通车》洛宁就业服务中心启动',
-        url: 'http://www.hndt.com/fm/1074/201709/14/1905363/res/Hj92Xm6P.jpg'
+        text: '私家车999就来此购美食商城正式上线',
+        url: 'http://www.hndt.com/fm/999/201708/04/1897366/res/gKN9OcmA.png?1502679979079'
     },
     {
-        text: '河南广电联手三甲医院助力医疗扶贫 免费治疗患者出院',
-        url: 'http://www.hndt.com/fm/1074/201708/31/1902584/res/DOrUk7XF.jpg'
+        text: '河南财经政法大学文化传播学院在私家车广播建立实习基地',
+        url: 'http://www.hndt.com/fm/999/201711/01/1914824/res/3VhuZuuX.jpg?1509507550259'
     },
     {
-        text: '中国·哈密“甜蜜之旅”第十四届哈密瓜节开幕',
-        url: 'http://www.hndt.com/fm/1074/201708/14/1898815/res/MyizLTDb.jpg'
+        text: '私家车999“9要甜蜜蜜，5所畏惧”生日大party',
+        url: 'http://www.hndt.com/fm/999/1904713/res/cfGnUOPs.jpg'
     }
 ]
 
