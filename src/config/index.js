215cb4853a4e4744fd931ee7be2336709bd8141e
DESKTOP-73RF5IL\rm <763457149@qq.com>
2017-11-03 16:20:24 +0800

    信息广播

--- src/config/index.js
+++ src/config/index.js
@@ -1,48 +1,48 @@
 //配置文件
 //channelLogo
-const logoIndex = 8
+const logoIndex = 9
 const CHANNEL_LOGO = require(`../logo/${logoIndex}.png`)
     //封面
-const CHANNEL_TITLE = '教育广播';
+const CHANNEL_TITLE = '乐龄105.6';
 //one
 const CHANNEL_AUDIO = 'http://www.hndt.com/podcast/881/1026/201710/31/1914736/res/DXIxLN8m.mp3';
 
-const CHANNEL_AUDIO_TEXT_1 = '河南教育广播【Up-radio1066】隶属于河南广播电视台，调频覆盖河南全省，郑州播出频率FM106.6。';
+const CHANNEL_AUDIO_TEXT_1 = '河南广播电视台信息广播（FM105.6/AM603），前身为河南信息广播电台，开播于1994年6月8日，凭借调频、中波、卫星、网络，覆盖河南全省及周边3亿多听众，是中原地区极具成长性的广播媒体。';
 
-const CHANNEL_AUDIO_TEXT_2 = '在知识叠代爆发的年代，学习伴随终身，Up-Radio1066让你车上的时间不再无聊，用有趣的方式打开知识。随时打开1066，你能听到全国最红音频大v的有声知识；你能听到音乐和知识伴随，知识从此不再枯燥；你能听到本土最具知识的牛人分享；你能听到最新潮的学习风尚。';
+const CHANNEL_AUDIO_TEXT_2 = '2015年7月，信息广播改版升级为信息广播“乐龄105.6”，开创全国省级中老年广播先河。';
 
 //two
 const CHANNEL_VIDEO = 'http://hndt.com/fm/1066/201708/07/1897792/res/vk2yJveQ.mp4';
 
-const CHANNEL_VIDEO_TEXT_1 = '集纳全国最红知识音频IP。罗振宇、梁宏达、吴晓波、高晓松、李翔、许树泽、富赜、老沈，共在1066发声，全天十五档知识单元与精美音乐混排轮盘，随时打开1066，随时有知识和音乐伴随。';
+const CHANNEL_VIDEO_TEXT_1 = '以“点亮多彩生活，陪伴不老青春”为口号，以“年轻态”为频率风格主调，全方位打造国内中老年广播第一品牌。“乐龄105.6”节目设置以“新闻资讯、生活服务、文艺娱乐”为主线，从生活保障、文化娱乐，到精神抚慰、时尚引导，全方位服务中老年人群。';
 
 //three
 const CHANNEL_PICS = [{
-        text: '《郑在读诗》“喜迎十九大 巾帼心向党”故事朗诵会圆满落幕！',
-        url: 'http://www.hndt.com/fm/1066/201710/18/1912026/res/6s3BG8Qo.jpg'
+        text: '新时代新乐龄！ 乐龄1056情暖重阳系列活动开始啦',
+        url: 'http://www.hndt.com/fm/1056/201710/24/1913444/res/dvHJN7gg.jpg'
     },
     {
-        text: '遇见 ｜九月，和教育广播一起相约郑州最美幼教人！',
-        url: 'http://www.hndt.com/fm/1066/201710/23/1912973/res/Lf9TTmjn.jpg'
+        text: '“中原乐龄展风采 喜迎党的十九大”活动完美收官！',
+        url: 'http://www.hndt.com/fm/1056/201709/22/1906988/res/69EdxlxK.jpg'
     },
     {
-        text: '寻师问道·对话恩师，这只是一个开始......',
-        url: 'http://www.hndt.com/fm/1066/201709/07/1904052/res/H3kUYLnR.jpg'
+        text: '《名医在线》联合郑大一附院举行大型义诊活动',
+        url: 'http://www.hndt.com/fm/1056/201710/26/1913813/res/N6ZzekmJ.jpg'
     }
 ]
 
 //four
 const CHANNEL_PICS_2 = [{
-        text: '萌宝、读诗、玩直播，这是一次任性的车展。',
-        url: 'http://www.hndt.com/fm/1066/201708/14/1898865/res/qIJ0mg7e.jpg?1502699896957'
+        text: '新时代，新乐龄！“乐游明清古村”听众报名火爆进行中',
+        url: 'http://www.hndt.com/fm/1056/201710/24/1913426/res/P9kIZtri.jpg'
     },
     {
-        text: '【二姐驾到】不简单！零基础，也可以这么赞！',
-        url: 'http://www.hndt.com/fm/1066/201708/11/1898704/res/6SstlF2w.jpg?1502447527233'
+        text: '“我是小小急救员” 暑期急救自救技能大练兵',
+        url: 'http://www.hndt.com/fm/1056/201709/03/1903238/res/fsEssF0i.jpg'
     },
     {
-        text: '《郑在读诗》摩洛哥之旅，走进三毛笔下的撒哈拉！',
-        url: 'http://www.hndt.com/fm/1066/201711/01/1914916/res/qcGdGL8v.jpg?1509527639873'
+        text: '红姐配餐进社区，做蛋糕这事，不难，很快乐！',
+        url: 'http://www.hndt.com/fm/1056/201709/03/1903236/res/FGEBjLuB.jpg'
     }
 ]
 
