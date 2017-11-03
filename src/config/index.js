4690c65b51c878c889f31e36eb1f695c61cbeec7
DESKTOP-73RF5IL\rm <763457149@qq.com>
2017-11-03 14:45:44 +0800

    音乐广播

--- src/config/index.js
+++ src/config/index.js
@@ -1,54 +1,50 @@
 //配置文件
 //channelLogo
-const logoIndex = 3
+const logoIndex = 4
 const CHANNEL_LOGO = require(`../logo/${logoIndex}.png`)
     //封面
-const CHANNEL_TITLE = '河南戏曲广播';
+const CHANNEL_TITLE = '河南音乐广播';
 //one
 const CHANNEL_AUDIO = 'http://www.hndt.com/podcast/881/1026/201710/31/1914736/res/DXIxLN8m.mp3';
 
-const CHANNEL_AUDIO_TEXT_1 = '“娱乐976”前身为河南电台戏曲广播，2014年元月1日转型改主打娱乐，拥有巨大的影响力：2016年5月 72小时好戏唱不停•挑战世界纪录；2016年9月 中国豫剧首创吉尼斯世界纪录；连续两届“河之南”文化娱乐风尚盛典；公益品牌“大石桥有戏”“福塔有戏”“欧安乐龄有戏”已成功举办150余期，累计观众100万＋；开拍“中原历史文化名人少儿系列剧”；成立“月阳工作室”......';
-
+const CHANNEL_AUDIO_TEXT_1 = '从黑胶唱片出发，走过卡带岁月，经历CD年代，再到听MP3的日子……音乐的陪伴始终不曾离开，魅力881河南音乐广播，改变的是聆听方式，不变的是打动心灵的歌声。2003年5月18日，魅力881河南乐广播的诞生，不仅帮我们拾回被遗忘的时光，还改变了我们听歌的品质。如今我们依然停靠在左岸，调频向左，深度陪伴。聆听品质音乐，享受品质生活......';
 
 const CHANNEL_AUDIO_TEXT_2 = '';
 
 //two
-const CHANNEL_VIDEO = 'http://www.hndt.com/fm/976/201708/07/1897785/res/NPvFUuAS.mp4';
+const CHANNEL_VIDEO = 'http://www.hndt.com/fm/881/201708/07/1897786/res/CfRuH4bI.mp4';
 
-const CHANNEL_VIDEO_TEXT_1 = '娱乐广播转型至今在社会上产生广泛影响。赛立信数据调查显示：娱乐976全省收听率稳居前五；而郑州地区车载收听调查权威公司尼尔森调查显示：娱乐976排名进入前十。';
-
+const CHANNEL_VIDEO_TEXT_1 = '魅力881音乐广播，已实现全省覆盖：郑州/开封/漯河/周口/三门峡市市[FM88.1]；新乡市 [FM88.5]；南阳市[FM88.8]；洛阳市/焦作市[FM89.7]；鹤壁市/濮阳市[FM95.0];驻马店市/信阳市/平顶山市[FM95.8]；安阳市[FM98.3]； 许昌市[FM100.4]。';
 
 //three
 const CHANNEL_PICS = [{
-        text: '月阳工作室一周岁啦！“好戏连台”邀您品赏祥符陈韵！',
-        url: 'http://www.hndt.com/fm/976/201708/11/1898681/res/ykIhMokA.jpg'
+        text: '魅力881丨国庆中秋齐欢聚，神农山景区门票免费送',
+        url: 'http://www.hndt.com/fm/881/201709/29/1908687/res/trWoZE4u.jpg?1506668515821'
     },
     {
-        text: '豫南曲剧名丑要来欧安乐龄有戏，本周六看曲剧经典《三子争父》',
-        url: 'http://www.hndt.com/fm/976/201711/02/1915091/res/lnjm54yM.jpg'
+        text: '2017国庆节甘北胡杨林沙漠穿越8日精品自驾之旅！',
+        url: 'http://www.hndt.com/fm/881/201709/22/1906985/res/aYnNxSeZ.jpg?1506070156081'
     },
     {
-        text: '976福塔有戏 & 中原百姓大舞台|这个重阳节，好戏连台等您来',
-        url: 'http://www.hndt.com/fm/976/201710/30/1914285/res/NDk3PmY8.jpg'
+        text: '《最爱金曲榜》开始投票，金曲单元节目即将播出',
+        url: 'http://www.hndt.com/fm/881/201709/11/1904699/res/LzwOqtk4.jpg?1505098026620'
     }
 ]
 
 //four
 const CHANNEL_PICS_2 = [{
-        text: '坠剧《小姑贤》“引爆”欧安乐龄有戏，内黄女笑星情牵郑州戏迷',
-        url: 'http://www.hndt.com/fm/976/201710/19/1912307/res/RMIczlOo.jpg'
+        text: '2017新豫商行读中原徒步活动',
+        url: 'http://www.hndt.com/fm/881/201708/14/1898818/res/Qs6m8xeI.jpg?1502676758812'
     },
     {
-        text: '我听广播看大戏|《泪洒相思地》王怜娟被逼跳入西湖还是太湖内',
-        url: 'http://www.hndt.com/fm/976/201710/11/1910275/res/RZFo9Ace.jpg'
+        text: 'WDG第五届 中国（郑州）国际街舞大赛总决赛完美落幕',
+        url: 'http://www.hndt.com/fm/881/201708/10/1898404/res/S2mblE1B.jpg?1502338116328'
     },
     {
-        text: '爱心少年行-爱逗小天使走进郑州儿童福利院',
-        url: 'http://www.hndt.com/fm/976/201708/24/1901092/res/EWvT0FsG.jpg'
+        text: '葡萄成熟时，我们满载而归啦！',
+        url: 'http://www.hndt.com/fm/881/201708/10/1898384/res/KBdq8lCT.jpg?1502331319496'
     }
 ]
-
-
 
 export {
     CHANNEL_LOGO,
