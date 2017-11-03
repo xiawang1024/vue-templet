287ee620ad688e975bd5b298c5c587bad8bd5dc6
DESKTOP-73RF5IL\rm <763457149@qq.com>
2017-11-03 15:10:14 +0800

    农村广播

--- src/config/index.js
+++ src/config/index.js
@@ -1,48 +1,49 @@
 //配置文件
 //channelLogo
-const logoIndex = 4
+const logoIndex = 5
 const CHANNEL_LOGO = require(`../logo/${logoIndex}.png`)
     //封面
-const CHANNEL_TITLE = '河南音乐广播';
+const CHANNEL_TITLE = '河南农村广播';
 //one
 const CHANNEL_AUDIO = 'http://www.hndt.com/podcast/881/1026/201710/31/1914736/res/DXIxLN8m.mp3';
 
-const CHANNEL_AUDIO_TEXT_1 = '从黑胶唱片出发，走过卡带岁月，经历CD年代，再到听MP3的日子……音乐的陪伴始终不曾离开，魅力881河南音乐广播，改变的是聆听方式，不变的是打动心灵的歌声。2003年5月18日，魅力881河南乐广播的诞生，不仅帮我们拾回被遗忘的时光，还改变了我们听歌的品质。如今我们依然停靠在左岸，调频向左，深度陪伴。聆听品质音乐，享受品质生活......';
+const CHANNEL_AUDIO_TEXT_1 = '农村广播是国家批准的政策性广播、公益性媒体。2015年9月1日全新改版，定位于“沟通城乡，服务三农”，服务城乡居民生产生活每一天。农村广播以互联网新媒体思维办节目，节目不但形式新颖，服务性、权威性强，而且线上节目播出，线下活动带动，节目一次采集、多元生成、多平台传播，是河南惟一一家面向“ 三农”、 专业服务7000万城乡人口的专业电台。';
 
 const CHANNEL_AUDIO_TEXT_2 = '';
 
 //two
-const CHANNEL_VIDEO = 'http://www.hndt.com/fm/881/201708/07/1897786/res/CfRuH4bI.mp4';
+const CHANNEL_VIDEO = 'http://www.hndt.com/fm/1074/201708/07/1897788/res/u9M50TSH.mp4';
 
-const CHANNEL_VIDEO_TEXT_1 = '魅力881音乐广播，已实现全省覆盖：郑州/开封/漯河/周口/三门峡市市[FM88.1]；新乡市 [FM88.5]；南阳市[FM88.8]；洛阳市/焦作市[FM89.7]；鹤壁市/濮阳市[FM95.0];驻马店市/信阳市/平顶山市[FM95.8]；安阳市[FM98.3]； 许昌市[FM100.4]。';
+const CHANNEL_VIDEO_TEXT_1 = '10多年来，农村广播紧扣“三农”需求，强化“农”字招牌，成为城乡农民生产生活的好帮手、好顾问、好参谋，为推动我省农业发展、社会和谐、建设美丽富裕新农村做出应有贡献。';
+
 
 //three
 const CHANNEL_PICS = [{
-        text: '魅力881丨国庆中秋齐欢聚，神农山景区门票免费送',
-        url: 'http://www.hndt.com/fm/881/201709/29/1908687/res/trWoZE4u.jpg?1506668515821'
+        text: '助力精准扶贫 专家服务团走进“第一书记”扶贫村',
+        url: 'http://www.hndt.com/fm/1074/201710/27/1913891/res/hiKYbqK9.jpg'
     },
     {
-        text: '2017国庆节甘北胡杨林沙漠穿越8日精品自驾之旅！',
-        url: 'http://www.hndt.com/fm/881/201709/22/1906985/res/aYnNxSeZ.jpg?1506070156081'
+        text: '栾川竟藏着这种好东西，十四国总理都为它点赞！',
+        url: 'http://www.hndt.com/fm/1074/201710/30/1914192/res/EW8lQhGx.jpg'
     },
     {
-        text: '《最爱金曲榜》开始投票，金曲单元节目即将播出',
-        url: 'http://www.hndt.com/fm/881/201709/11/1904699/res/LzwOqtk4.jpg?1505098026620'
+        text: '第一书记富民成果走进雏牧香 河南广电扶贫再发力',
+        url: 'http://www.hndt.com/fm/1074/201710/16/1911320/res/1aZCF3fB.jpg'
     }
 ]
 
 //four
 const CHANNEL_PICS_2 = [{
-        text: '2017新豫商行读中原徒步活动',
-        url: 'http://www.hndt.com/fm/881/201708/14/1898818/res/Qs6m8xeI.jpg?1502676758812'
+        text: '《打工直通车》洛宁就业服务中心启动',
+        url: 'http://www.hndt.com/fm/1074/201709/14/1905363/res/Hj92Xm6P.jpg'
     },
     {
-        text: 'WDG第五届 中国（郑州）国际街舞大赛总决赛完美落幕',
-        url: 'http://www.hndt.com/fm/881/201708/10/1898404/res/S2mblE1B.jpg?1502338116328'
+        text: '河南广电联手三甲医院助力医疗扶贫 免费治疗患者出院',
+        url: 'http://www.hndt.com/fm/1074/201708/31/1902584/res/DOrUk7XF.jpg'
     },
     {
-        text: '葡萄成熟时，我们满载而归啦！',
-        url: 'http://www.hndt.com/fm/881/201708/10/1898384/res/KBdq8lCT.jpg?1502331319496'
+        text: '中国·哈密“甜蜜之旅”第十四届哈密瓜节开幕',
+        url: 'http://www.hndt.com/fm/1074/201708/14/1898815/res/MyizLTDb.jpg'
     }
 ]
 
