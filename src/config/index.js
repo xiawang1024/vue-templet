ede6f374a7735b9c9fd31106780ace052380c100
DESKTOP-73RF5IL\rm <763457149@qq.com>
2017-11-06 09:41:10 +0800

    my radio 配置

--- src/config/index.js
+++ src/config/index.js
@@ -1,9 +1,9 @@
 //配置文件
 //channelLogo
-const logoIndex = 6
+const logoIndex = 5
 const CHANNEL_LOGO = require(`../logo/${logoIndex}.png`)
     //封面
-const CHANNEL_TITLE = '影视广播';
+const CHANNEL_TITLE = 'My Radio';
 //one
 const CHANNEL_AUDIO = 'http://www.hndt.com/podcast/881/1026/201710/31/1914736/res/DXIxLN8m.mp3';
 
@@ -12,12 +12,12 @@ const CHANNEL_AUDIO_TEXT_1 = '开播于2004年4月10日的My Radio是覆盖河�
 const CHANNEL_AUDIO_TEXT_2 = '';
 
 //two
-const CHANNEL_VIDEO = 'http://hndt.com/fm/900/201708/07/1897789/res/xGGUNxmt.mp4';
+const CHANNEL_VIDEO = 'http://hndt.com/h5/shows/05/videos/1.mp4';
 
 const CHANNEL_VIDEO_TEXT_1 = '24小时流行音乐不间断播出，节目以单元化、碎片化的轮盘状态呈现，成为听众无压力、最亲密的声音陪伴者。';
 
 //twoOne
-const CHANNEL_VIDEO_ONE = 'http://hndt.com/h5/shows/06/videos/2.mp4';
+const CHANNEL_VIDEO_ONE = 'http://hndt.com/h5/shows/05/videos/2.mp4';
 
 const CHANNEL_VIDEO_TEXT_1_ONE = '周周有活动，月月有主题。明星演唱会、荒岛音乐会、先锋音乐会、音符小精灵、潮流创意文化节、山谷音乐节、快乐广播站等,每年上百场的系列活动充分彰显了团队的执行力和创新意识。';
 
