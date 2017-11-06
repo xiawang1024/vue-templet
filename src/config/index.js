795b0a7dfefb70885e6f0cadeb384815b90213c7
DESKTOP-73RF5IL\rm <763457149@qq.com>
2017-11-06 15:47:13 +0800

    修复

--- src/config/index.js
+++ src/config/index.js
@@ -7,14 +7,14 @@ const CHANNEL_TITLE = '河南广播云平台';
 //one
 const CHANNEL_AUDIO = 'http://www.hndt.com/podcast/881/1026/201710/31/1914736/res/DXIxLN8m.mp3';
 
-const CHANNEL_AUDIO_TEXT_1 = '河南广播网是河南广播电视台广播业务领域的官方网站， 聚合了河南广播电视台10套广播频率、 14 套网络广播、 河南18个省辖市及各县市100多套广播频率资源。“ 听河南， 览天下”， 旨在打造“ 集音视频、 网络互动于一体的网络收听平台”。';
+const CHANNEL_AUDIO_TEXT_1 = '河南广播网是河南广播电视台广播业务领域的官方网站，聚合了河南广播电视台10套广播频率、14 套网络广播、河南18个省辖市及各县市100多套广播频率资源。“ 听河南，览天下”，旨在打造“ 集音视频、网络互动于一体的网络收听平台”。';
 
-const CHANNEL_AUDIO_TEXT_2 = '河南广播网手机版、 河南广播官方微博、 河南广播微信公众号、 河南广播小程序， 多种传播渠道让您随时随地看新闻、 听广播！';
+const CHANNEL_AUDIO_TEXT_2 = '河南广播网手机版、河南广播官方微博、河南广播微信公众号、河南广播小程序，多种传播渠道让您随时随地看新闻、听广播！';
 
 //two
 const CHANNEL_VIDEO = 'http://hndt.com/h5/shows/10/videos/1.mp4';
 
-const CHANNEL_VIDEO_TEXT_1 = '自主研发的河南广播APP， 为广大用户提供河南本地广播节目的网络直播、 节目点播、 直播互动、 主持人播客、 社区圈层等服务。';
+const CHANNEL_VIDEO_TEXT_1 = '自主研发的河南广播APP，为广大用户提供河南本地广播节目的网络直播、节目点播、直播互动、主持人播客、社区圈层等服务。';
 
 //three
 const CHANNEL_PICS = [{
