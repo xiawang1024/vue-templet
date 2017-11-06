9f1c9d62c79b0f2b39747c30ce187c1f1fd32d0e
DESKTOP-73RF5IL\rm <763457149@qq.com>
2017-11-06 14:27:35 +0800

    分享logo

--- static/js/wxAutoPlay.js
+++ static/js/wxAutoPlay.js
@@ -1,7 +1,7 @@
 window.onload = function() {
-    var TITLE = '2018推介会'
+    var TITLE = '2018河南广播电视台广播品牌推介会'
     var LINK = 'http://hndt.com/h5/shows/08/index.html'
-    var IMGURL = 'http://hndt.com/res/logo_300.png'
+    var IMGURL = 'http://www.hndt.com/h5/shows/sharelogos/8.png'
     var DESC = '2018河南广播电视台广播品牌推介会'
 
     //微信配置
