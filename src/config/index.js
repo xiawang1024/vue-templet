ad92975573e83aec8c10eda44e0f7671b7771def
DESKTOP-73RF5IL\rm <763457149@qq.com>
2017-11-06 09:11:04 +0800

    视频配置地址

--- src/config/index.js
+++ src/config/index.js
@@ -12,7 +12,7 @@ const CHANNEL_AUDIO_TEXT_1 = '';
 const CHANNEL_AUDIO_TEXT_2 = '新闻广播全天候贯穿整点新闻和服务资讯，是资讯的高地，民意的窗口，担负着党委政府政令传达、舆论引导的重要政治任务。主要听众为20岁-50岁的政府官员、企业管理者、公务员、白领、高校学者、知识分子。他们关注新闻，他们热爱活动，丰富及时的资讯使他们在运筹帷幄中更添自信和从容，新闻广播是受众收听倾向最为强烈的广播媒体之一。';
 
 //two
-const CHANNEL_VIDEO = 'http://www.hndt.com/fm/954/201708/07/1897761/res/1uTCq0ay.mp4';
+const CHANNEL_VIDEO = 'http://www.hndt.com/h5/shows/01/videos/1.mp4';
 
 const CHANNEL_VIDEO_TEXT_1 = '新闻广播目前采用中波657、调频95.4、调频102.3三频发射，全省覆盖率达96%以上。其中中波300千瓦的发射率，让新闻广播傲视群雄，响彻中原！';
 
