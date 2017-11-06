10c695c217082b93f2441d9cf145ec13d89df2db
DESKTOP-73RF5IL\rm <763457149@qq.com>
2017-11-06 09:29:29 +0800

    农村广播配置

--- src/config/index.js
+++ src/config/index.js
@@ -3,7 +3,7 @@
 const logoIndex = 5
 const CHANNEL_LOGO = require(`../logo/${logoIndex}.png`)
     //封面
-const CHANNEL_TITLE = '河南农村广播';
+const CHANNEL_TITLE = '农村广播';
 //one
 const CHANNEL_AUDIO = 'http://www.hndt.com/podcast/881/1026/201710/31/1914736/res/DXIxLN8m.mp3';
 
